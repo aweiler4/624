c56aeb45d972d77ed70b150c46275e3e3cdae07b
aweiler4 <102678755+aweiler4@users.noreply.github.com>
2022-06-24 13:54:13 -0400

    created handleOnchange

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -12,11 +12,17 @@ const Movies = () => {
         searchURL: ''
     })
 
+    const handleOnchange = (e) => {
+        setFormData({...formData, movieTitle: e.target.value})
+        console.log(e.target.value)
+    }
 
     return(
         <form>
             <label htmlFor='search'>Search Title </label>
-            <input value='' id='search' type='text' name='search' placeholder='Search for Movies'></input>{' '}
+            <input onChange={(e) => {
+                handleOnchange(e)
+            }} value={formData.movieTitle} id='search' type='text' name='search' placeholder='Search for Movies'></input>{' '}
             <input type='submit'></input>
         </form>
     )
